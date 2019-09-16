2de8092f5aebf85674f825a8807551159aeb92bd
GabrielaZarzozaPerez <55330340+GabrielaZarzozaPerez@users.noreply.github.com>
2019-09-16 16:18:09 -0500

    Create HolaMundo.py

---
+++ HolaMundo.py
@@ -0,0 +1,10 @@
+# HolaMundo.py
+# Autor: Gabriela Zarzoza Perez
+# Fecha de creacion: 16/09/2019
+
+# Los comentarios se realizan con #. El resto de 
+# la linea no tiene significado para el resto del interprete.
+
+# print genera una salida de consola. El elemento 
+# a imprimir debe delimitarse entre parentesis.
+print("Hola Mundo, ahora en python!")
